fdd608310e62ca4141d2fabb24fc1b527fb98d01
Alejandro Capel <alejandro_capel@hotmail.com>
2021-01-22 04:48:28 +0100

    Update gettranslate.py

--- todos/gettranslate.py
+++ todos/gettranslate.py
@@ -10,12 +10,41 @@ def gettranslate(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
     # fetch todo from the database
-    result = table.get_item(
+    result1 = table.get_item(
         Key={
             'id': event['pathParameters']['id']
         }
     )
 
+    # translate -------------------------------
+    #1 - Recover text to translate
+    print  "1 - Recover text to translate"
+    print   "result1 : " + result1
+    
+    texto = ''
+    json_object = json.loads(result1)
+    pairs = json_object.items()
+
+    for key, value in pairs:
+       if (key == 'text' ) texto = value
+    
+    if texto == ''  
+        print("NO ha encontrado el campo text en el registro desde BBDD.")
+        return
+    
+    #2 - translate text
+    translate = boto3.client(service_name='translate', region_name='region', use_ssl=True)
+    if ( event['pathParameters']['language'] = 'FR')
+        result1 = translate.translate_text(Text=texto, SourceLanguageCode="en", TargetLanguageCode="fr")
+    else
+        result1 = translate.translate_text(Text=texto, SourceLanguageCode="en", TargetLanguageCode="es")
+    print("result1 : " + result1)
+        
+    #3 - change language 
+    result = string.replace(texto, result1)
+    print("result : " + result)
+        
+        
     # create a response
     response = {
         "statusCode": 200,
